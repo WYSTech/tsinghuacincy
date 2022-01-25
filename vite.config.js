dcb29f9fc5199eb91a7c5b389df221181a68f1e7
wei.wang <wei.wang@39304WW>
2022-01-25 15:50:20 -0500

    local path

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  base: '/tsinghuacincy/'
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
